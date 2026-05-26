57f4824076b19d87f424b1fd92a6780c54be1a9d
TEKNEI\sgantolin <sgantolin@teknei.com>
2026-05-26 12:22:00 +0200

    eliminar login

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,11 @@ import Header from './shared/components/Header';
 import BreadCrumb  from "./shared/components/Breadcrumb";
 import Footer from './shared/components/Footer';
 
-import Login from "./pages/login/Login";
-import ChangePassword from "./pages/login/ChangePassword";
-import Autentification from "./pages/login/Autentification";
-import RolSelection from "./pages/login/RolSelection";
-
 
 function App() {
     return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-          <Route path="/autentification" element={<Autentification />} />
-          <Route path="/seleccionar-rol" element={<RolSelection />} />
-          <Route path="/*" element={<MainLayout />} />
-        </Routes>
-      
+        <MainLayout />
       </BrowserRouter>
     )
 }
