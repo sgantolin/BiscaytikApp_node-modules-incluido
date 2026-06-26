c6950cc16217704a53f3c5fc242423e409fe39bd
TEKNEI\sgantolin <sgantolin@teknei.com>
2026-06-26 08:25:29 +0200

    pequeño ajuste para quitar margen card

--- src/shared/components/CardContainer.jsx
+++ src/shared/components/CardContainer.jsx
@@ -7,7 +7,7 @@ function CardContainer({ cards = [], direction = 'row', layout = 'vertical', car
 
   >
    {cards.map((card, i) => (
-    <li key={i} className={`BKTT-CardContainer__item col ${direction === 'column' ? 'col-12 mb-3' : ''}`}>
+    <li key={i} className={`BKTT-CardContainer__item col ${direction === 'column' ? 'col-12' : ''}`}>
      <div className={`BKTT-CardContainer__card card ${layout === 'horizontal' ? 'BKTT-CardContainer__card--horizontal' : ''} ${!card.image ? 'BKTT-CardContainer__card--no-image' : ''}`}>
 
       {(card.image || card.badgeText) && (
